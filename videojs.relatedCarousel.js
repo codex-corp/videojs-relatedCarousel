592d5524bf21e6591bee80566c50bc0e12fa5fe6
alex-phillips <ahp118@gmail.com>
2015-11-17 13:22:28 -0500

    modified code so that 'carousel' is an object attached to the player object for easy acess, added callback functionality to each carousel item

--- videojs.relatedCarousel.js
+++ videojs.relatedCarousel.js
@@ -29,102 +29,174 @@
     var player = this,
       settings = extend([], defaults, options || []);
 
-    var holderDiv = document.createElement('div');
-    holderDiv.className = 'vjs-related-carousel-holder';
+    var carousel = {
+      controlBarButton: document.createElement('div'),
 
-    var title = document.createElement('h5');
-    title.innerHTML = 'More Videos';
-    holderDiv.appendChild(title);
+      holderDiv: document.createElement('div'),
+      title: document.createElement('h5'),
 
-    player.el().appendChild(holderDiv);
+      viewport: document.createElement('div'),
+      items: document.createElement('ul'),
 
-    var carouselViewport = document.createElement('div');
-    carouselViewport.className = 'vjs-carousel-viewport';
-    holderDiv.appendChild(carouselViewport);
+      leftButton: document.createElement('div'),
+      leftButtonContent: document.createElement('div'),
 
-    var carouselItems = document.createElement('ul');
-    carouselItems.className = 'carousel-items';
-    carouselViewport.appendChild(carouselItems);
+      rightButton: document.createElement('div'),
+      rightButtonContent: document.createElement('div'),
 
-    var numItems = settings.length;
-    var imageWidthPercentage = 90 / numItems;
-
-    // Render carousel buttons
-    var leftButton = document.createElement('div');
-    leftButton.className = 'vjs-carousel-left-button';
-    var leftButtonContent = document.createElement('div');
-    leftButtonContent.className = 'icon-videojs-carousel-left';
-    leftButton.appendChild(leftButtonContent);
-    holderDiv.appendChild(leftButton);
-
-    var rightButton = document.createElement('div');
-    rightButton.className = 'vjs-carousel-right-button';
-    var rightButtonContent = document.createElement('div');
-    rightButtonContent.className = 'icon-videojs-carousel-right';
-    rightButton.appendChild(rightButtonContent);
-    holderDiv.appendChild(rightButton);
-
-    var currentPosition = 0;
-    var maxPosition = (-176) * (settings.length - 1);
-    leftButton.onclick = function() {
-      if (currentPosition === 0) {
-        return;
-      }
-      currentPosition = currentPosition + 176;
-      carouselItems.style.left = currentPosition + 'px';
-    };
-
-    rightButton.onclick = function() {
-      if (currentPosition <= maxPosition) {
-        return;
-      }
-      currentPosition = currentPosition - 176;
-      carouselItems.style.left = currentPosition + 'px';
-    };
-
-    for (var i = 0; i < numItems; i++) {
-      var item = document.createElement('li');
-      item.className = 'carousel-item';
-
-      var img = document.createElement('img');
-      img.src = settings[i].imageSrc;
-      img.className = 'vjs-carousel-thumbnail';
-      img.alt = settings[i].title;
-      img.style.width = '100%';
-
-      var anchor = document.createElement('a');
-      anchor.href = settings[i].url;
-      anchor.appendChild(img);
-      anchor.title = settings[i].title;
-
-      var title = document.createElement('span');
-      title.innerHTML = settings[i].title;
-      anchor.appendChild(title);
-
-      item.appendChild(anchor);
-      carouselItems.appendChild(item);
-
-      player.on('mouseout', function() {
-        if (!holderDiv.className.match(/vjs-fade-out/)) {
-          holderDiv.className = holderDiv.className + " vjs-fade-out";
+      config: null,
+      currentPosition: 0,
+      maxPosition: 0,
+      currentVideoIndex: -1,
+      isOpen: false,
+      open: function() {
+        if (!carousel.holderDiv.className.match(/active/)) {
+          carousel.holderDiv.className = carousel.holderDiv.className + " active";
         }
-      });
-      player.on('mouseover', function() {
-        holderDiv.className = holderDiv.className.replace(/\s*vjs-fade-out\s*/g, '');
-      });
-    }
+        this.isOpen = true;
+      },
+      close: function() {
+        if (carousel.holderDiv.className.match(/active/)) {
+          carousel.holderDiv.className = carousel.holderDiv.className.replace(/\s*active\s*/, '');
+        }
+        this.isOpen = false;
+      },
+      toggle: function() {
+        if (this.isOpen) {
+          this.close();
+        } else {
+          this.open();
+        }
+      },
+
+      initiateVideo: function(index, config, trigger) {
+        currentVideoIndex = index;
+        if (config.callback !== undefined) {
+          config.callback(player, config, {
+            trigger: trigger,
+            newIndex: currentVideoIndex
+          });
+        } else {
+          carousel.close();
+          if (config.src !== undefined) {
+            player.src(config.src);
+            player.play();
+          } else {
+            window.location = config.url;
+          }
+        }
+      },
+
+      onItemClick: function(index, element, config) {
+        element.onclick = function(e) {
+          e.preventDefault();
+          carousel.initiateVideo(index, config, e);
+        };
+      },
+
+      buildCarousel: function(config) {
+        this.config = config;
+        this.items.innerHTML = '';
+        this.maxPosition = (-176) * (this.config.length - 1)
+
+        // Initialize carousel items
+        for (var i = 0; i < this.config.length; i++) {
+          var item = document.createElement('li');
+          item.className = 'carousel-item';
+
+          var img = document.createElement('img');
+          img.src = this.config[i].imageSrc;
+          img.className = 'vjs-carousel-thumbnail';
+          img.alt = this.config[i].title;
+          img.style.width = '100%';
+
+          var anchor = document.createElement('a');
+
+          if (!this.config[i].url) {
+            this.config[i].url = '#';
+          }
+
+          anchor.href = this.config[i].url;
+          anchor.title = this.config[i].title;
+          anchor.appendChild(img);
+
+          this.onItemClick(i, anchor, this.config[i]);
+
+          var title = document.createElement('span');
+          title.innerHTML = this.config[i].title;
+          anchor.appendChild(title);
+
+          item.appendChild(anchor);
+          this.items.appendChild(item);
+        }
+      }
+    };
 
     /* Menu Button */
-    var RelatedCarouselButton = document.createElement('div');
-    RelatedCarouselButton.className = 'vjs-button vjs-control vjs-related-carousel-button icon-videojs-carousel-toggle';
-    RelatedCarouselButton.onclick = function(e) {
-      if (holderDiv.className.match(/active/)) {
-        holderDiv.className = holderDiv.className.replace(/\s*active\s*/, '');
-      } else {
-        holderDiv.className = holderDiv.className + " active";
+    carousel.controlBarButton.className = 'vjs-button vjs-control vjs-related-carousel-button icon-videojs-carousel-toggle';
+
+    carousel.holderDiv.className = 'vjs-related-carousel-holder';
+    carousel.title.innerHTML = 'More Videos';
+    carousel.viewport.className = 'vjs-carousel-viewport';
+    carousel.items.className = 'carousel-items';
+    carousel.leftButton.className = 'vjs-carousel-left-button';
+    carousel.leftButtonContent.className = 'icon-videojs-carousel-left';
+    carousel.rightButton.className = 'vjs-carousel-right-button';
+    carousel.rightButtonContent.className = 'icon-videojs-carousel-right';
+
+    // Add all items to DOM
+    player.controlBar.el().appendChild(carousel.controlBarButton);
+    carousel.holderDiv.appendChild(carousel.title);
+    player.el().appendChild(carousel.holderDiv);
+    carousel.holderDiv.appendChild(carousel.viewport);
+    carousel.viewport.appendChild(carousel.items);
+    carousel.leftButton.appendChild(carousel.leftButtonContent);
+    carousel.holderDiv.appendChild(carousel.leftButton);
+    carousel.rightButton.appendChild(carousel.rightButtonContent);
+    carousel.holderDiv.appendChild(carousel.rightButton);
+
+    // Add event handlers
+    carousel.controlBarButton.onclick = function(e) {
+      carousel.toggle();
+    };
+    carousel.leftButton.onclick = function() {
+      if (carousel.currentPosition === 0) {
+        return;
       }
+      carousel.currentPosition = carousel.currentPosition + 176;
+      carousel.items.style.left = carousel.currentPosition + 'px';
     };
 
-    player.controlBar.el().appendChild(RelatedCarouselButton);
+    carousel.rightButton.onclick = function() {
+      if (carousel.currentPosition <= carousel.maxPosition) {
+        return;
+      }
+      carousel.currentPosition = carousel.currentPosition - 176;
+      carousel.items.style.left = carousel.currentPosition + 'px';
+    };
+
+    carousel.buildCarousel(settings);
+
+    player.carousel = carousel;
+
+    // Player events
+    player.on('mouseout', function() {
+      if (!carousel.holderDiv.className.match(/vjs-fade-out/)) {
+        carousel.holderDiv.className = carousel.holderDiv.className + " vjs-fade-out";
+      }
+    });
+    player.on('mouseover', function() {
+      carousel.holderDiv.className = carousel.holderDiv.className.replace(/\s*vjs-fade-out\s*/g, '');
+    });
+    player.on('timeupdate', function() {
+      if (player.ended()) {
+        if (carousel.currentVideoIndex === carousel.config.length) {
+          return;
+        }
+
+        carousel.currentVideoIndex++;
+        carousel.initiateVideo(carousel.currentVideoIndex, carousel.config[carousel.currentVideoIndex], player);
+      }
+    });
   });
 }(window.videojs));
